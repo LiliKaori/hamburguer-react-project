a8d7c8e113e441eacd84c4c07c87b65a9dae6389
LiliKaori <lilikaori10@gmail.com>
2023-03-23 19:05:51 -0300

    Começando projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { Container, Image, H1, ContainerItems, InputLabel, Input, Button } from "./style"
+import Burger from "./assets/burger.svg"
+import axios from "axios"
 
-function App() {
+function App(){  
+  const [orders, setOrders] = useState([])
+  const inputRequest = useRef()
+  const inputClientName = useRef()
+
+  async function addNewOrder(){
+    const newOrder = await axios.post("http://localhost:3001/order", {order: inputRequest.current.value, clientName: inputClientName.current.value})
+    // setOrders([{order: "big mac", clientName: "Liliam"}])
+    
+    setOrders([...orders, newOrder.data])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <div>
+        <Image src={Burger}/>
+        <H1>Faça seu pedido!</H1>
+      </div>
+
+      <ContainerItems>
+
+        <InputLabel>Pedido</InputLabel>
+        <Input ref={inputRequest} placeholder="1 Coca-cola, 1 X-salada"></Input>
+
+        <InputLabel>Nome do cliente</InputLabel>
+        <Input ref={inputClientName} placeholder="Steve Jobs"></Input>
+
+        <Button onClick={addNewOrder}>Novo Pedido</Button>
+        <ul>
+          {
+            orders.map((order) => (
+              <li key={order.id}>
+                {order.order}, {order.clientName}
+              </li>
+          ))}
+        </ul>
+
+      </ContainerItems>
+      
+    </Container>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,77 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    background-color: #0A0A10;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    gap: 25px;
+`
+
+export const Image = styled.img`
+    height: 354px;
+    margin: 11px 36px;
+`
+
+export const H1 = styled.h1`    
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 33px;
+    letter-spacing: 0px;
+    text-align: center;
+`
+
+export const ContainerItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 414px;
+    height: 100vh;
+    padding: 51px 36px;
+`
+
+export const InputLabel = styled.label`
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 22px;
+    letter-spacing: -0.40799999237060547px;
+    text-align: left;
+`
+
+export const Input = styled.input`
+    height: 58px;
+    border-radius: 14px;
+    background: rgba(255, 255, 255, 0.25);
+    padding: 19px 15px;
+    font-size: 18px;
+    font-weight: 300;
+    line-height: 21px;
+    letter-spacing: 0px;
+    text-align: left;
+    border: none;
+    margin-bottom: 42px;
+`
+
+export const Button = styled.button`
+    height: 68px;
+    background-color: #D93856;
+    font-size: 17px;
+    font-weight: 900;
+    line-height: 3px;
+    letter-spacing: 0px;
+    text-align: center;
+    margin-top: 76px;
+    cursor: pointer;
+
+    &:hover{
+        opacity: 0.9;
+    }
+
+    &:active{
+        opacity: 0.7;
+    }
+
+
+
+`
